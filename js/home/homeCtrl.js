8ed38788d8a7b32f2b748beb3d60cc07eb6caf5b
tsingleton5 <tjsingleton5@gmail.com>
2016-06-06 13:49:29 -0600

    all done

---
+++ js/home/homeCtrl.js
@@ -0,0 +1,5 @@
+angular.module('miniRouting').controller('homeCtrl', function ($scope) {
+
+// this one won't do anything but needs to have this basic data
+
+})
---
+++ js/settings/settingsCtrl.js
@@ -0,0 +1,5 @@
+angular.module('miniRouting').controller('settingsCtrl', function ($scope) {
+
+// this one won't do anything but needs to have this basic data
+
+})
